59bbed7ed4ec8ee6aea96bdfd217fd7123f1d378
Jothikumar <35065074+Jothikumarsk@users.noreply.github.com>
2025-01-13 10:43:12 +0530

    Update Contactform.js

--- client/src/components/Contactform.js
+++ client/src/components/Contactform.js
@@ -18,7 +18,7 @@ const ContactForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/submit-form', {
+      const response = await fetch('https://services-j2lq.onrender.com/submit-form', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
